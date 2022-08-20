0a700da826a5dd7557f1a506af683de7b37297dc
BrandonSosa10 <sos22994@gmail.com>
2022-08-20 03:05:14 -0600

    Me dio error 403 pero lo arregle ahora añadí el vet voluntario

---
+++ VetPrincipal.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.vetprincipal;
+import java.util.Random;
+import java.util.Scanner;
+
+public class VetPrincipal {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Elperro = new Perrito();
+        String respuestaPerrito = "";
+       
+        LaPersona = new Persona();
+        int acaricias = 0 // implementar las caricias puntos con el perro podria ser y..
+        // crear otra para las galletas aquellas
+         // un int de post
+        int postGalleta = 0;
+        
+        Random Rand = new Random();
+        
+        // ahora los mensajes emergentes para el output
+        //hacerlo tipo menu 
+        System.out.println("Veterinaria de voluntario"); // algo asi jeja
+        Sout ///// aaaa ano me va a dar tiempo :(
+        
+    }
+}
